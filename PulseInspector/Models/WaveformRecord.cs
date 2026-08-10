b77d7c908db21c481c630f9bfa30aed519cf4bf9
yochansung-tech <yochan.sung@gmail.com>
2026-08-10 20:03:38 +0900

    Store sampling metadata with each waveform record

--- PulseInspector/Models/WaveformRecord.cs
+++ PulseInspector/Models/WaveformRecord.cs
@@ -6,6 +6,9 @@ public sealed class WaveformRecord
     public string SourceName { get; init; } = string.Empty;
     public double[] Samples { get; init; } = Array.Empty<double>();
     public FeatureVector Features { get; init; } = new();
+    public double SampleIntervalSeconds { get; init; }
+    public double MeasurementPeriodSeconds => SampleIntervalSeconds * Samples.Length;
+    public bool HasExplicitTimeAxis { get; init; }
 
     public int SampleCount => Samples.Length;
 }
