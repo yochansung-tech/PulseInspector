e90855c2872036d40784d32e8a2f0b2aecd225e6
yochansung-tech <yochan.sung@gmail.com>
2026-08-12 11:47:07 +0900

    Run inspection CSV exporter tests

--- PulseInspector.Tests/FeatureDeviationTests.cs
+++ PulseInspector.Tests/FeatureDeviationTests.cs
@@ -43,6 +43,7 @@ internal static class FeatureDeviationTests
 
         FeatureDeviationGridTests.Run();
         InspectionSelectionServiceTests.Run();
+        InspectionCsvExporterTests.Run();
     }
 
     private static double ComputeMahalanobisSquared(FeatureVector vector, InspectionModel model)
