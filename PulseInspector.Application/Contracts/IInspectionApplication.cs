12eca5ccaef3af41d62e9fcbfbfce74d26d2d764
yochansung-tech <yochan.sung@gmail.com>
2026-09-04 19:33:10 +0900

    feat(app): expose training validation boundary

--- PulseInspector.Application/Contracts/IInspectionApplication.cs
+++ PulseInspector.Application/Contracts/IInspectionApplication.cs
@@ -1,4 +1,5 @@
 using PulseInspector.Models;
+using PulseInspector.Services;
 
 namespace PulseInspector.Application.Contracts;
 
@@ -7,6 +8,7 @@ public interface IInspectionApplication
     FeatureVector ExtractFeatures(IReadOnlyList<double> samples, double sampleIntervalSeconds);
     GroupData LoadGroup(IEnumerable<string> filePaths, double sampleIntervalSeconds);
     IReadOnlyList<GroupData> LoadGroupsFromRows(IEnumerable<string> filePaths, double sampleIntervalSeconds);
+    TrainingValidationResult ValidateTraining(IEnumerable<GroupData> normalGroups);
     InspectionModel Train(IEnumerable<GroupData> normalGroups, double confidence);
     GroupInspectionResult Inspect(GroupData group, InspectionModel model, GroupDecisionPolicy decisionPolicy);
     IReadOnlyList<SubgroupInspectionResult> InspectSubgroups(GroupData group, InspectionModel model);
