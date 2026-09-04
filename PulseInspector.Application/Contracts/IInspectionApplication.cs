7508138e6e55a7b5b8ff8245a951c9114bf11531
yochansung-tech <yochan.sung@gmail.com>
2026-09-04 14:30:24 +0900

    refactor: expand inspection application boundary

--- PulseInspector.Application/Contracts/IInspectionApplication.cs
+++ PulseInspector.Application/Contracts/IInspectionApplication.cs
@@ -5,4 +5,9 @@ namespace PulseInspector.Application.Contracts;
 public interface IInspectionApplication
 {
     FeatureVector ExtractFeatures(IReadOnlyList<double> samples, double sampleIntervalSeconds);
+    GroupData LoadGroup(IEnumerable<string> filePaths, double sampleIntervalSeconds);
+    IReadOnlyList<GroupData> LoadGroupsFromRows(IEnumerable<string> filePaths, double sampleIntervalSeconds);
+    InspectionModel Train(IEnumerable<GroupData> normalGroups, double confidence);
+    GroupInspectionResult Inspect(GroupData group, InspectionModel model, GroupDecisionPolicy decisionPolicy);
+    IReadOnlyList<SubgroupInspectionResult> InspectSubgroups(GroupData group, InspectionModel model);
 }
