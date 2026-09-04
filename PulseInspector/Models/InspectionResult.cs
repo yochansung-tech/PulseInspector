13983abaac772f34f5e184cc0b0ceb3e0760608f
yochansung-tech <yochan.sung@gmail.com>
2026-09-04 20:02:52 +0900

    refactor(core): remove legacy InspectionResult source

--- PulseInspector/Models/InspectionResult.cs
+++
@@ -1,8 +0,0 @@
-namespace PulseInspector.Models;
-
-public sealed record InspectionResult(
-    bool IsDefect,
-    double MahalanobisDistance,
-    double Threshold,
-    FeatureVector Features,
-    string Message);
